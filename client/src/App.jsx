f526e7f554d2de41fe513532e0e876fe5db6a780
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-05 18:37:09 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Checkout from './components/checkout';
+import ProductListing from './components/productListing';
+import ShoppingCart from './components/shoppingCart';
 
 export default function App() {
   return (
-    <hi className="text-red-500">App</hi>
+    <div>
+      <Router>
+        <Routes>
+          <Route path={"/product-listing"} element={<ProductListing/>} />
+          <Route path={"/checkout"} element={<Checkout/>} />
+          <Route path={"/shopping-cart"} element={<ShoppingCart/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
