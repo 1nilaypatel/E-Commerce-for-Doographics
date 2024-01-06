70e8017bf1f5095c150b87963c6d67428466832f
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2024-01-06 15:45:40 +0530

    added counter, updates on adding product to cart

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Appbar() {
+  const totalItems = useSelector((state) => state.cart.totalItems);
+
   return (
     <header className="fixed top-0 w-full bg-gray-800 text-slate-200 z-50">
       <div className="flex justify-between items-center p-4 max-w-6xl mx-auto">
@@ -8,9 +11,14 @@ export default function Appbar() {
           <span>Fake </span>
           <span className='text-slate-400'>E-Commerce</span>
         </Link>
-        <ul className="flex gap-5 font-semibold">
-          <Link to="/shopping-cart" className="hover:underline">
-            My Cart
+        <ul className="flex gap-5 font-semibold items-center">
+          <Link to="/shopping-cart" className="relative hover:underline">
+            <span>My Cart</span>
+            {totalItems > 0 && (
+              <span className="bg-green-500 text-white px-2 py-1 rounded-full absolute -top-1 -right-7">
+                {totalItems}
+              </span>
+            )}
           </Link>
         </ul>
       </div>
